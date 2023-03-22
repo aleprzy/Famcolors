eb84e25b3fed394660f04055e74839f829f8f039
aleprzy <aprzybylo98@gmail.com>
2023-03-22 15:30:03 +0100

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import ColorFilter from "./ColorFilter";
 import { ColumnContext } from "../contexts/ColumnContext";
 import { PaintingsVisibilityContext } from "../contexts/PaintingsVisibilityContext";
